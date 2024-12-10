1560ffeb4de7712cea5ea45af1d32bfe343cd354
Abdulkadir Sattar <abdulkadirsttr@gmail.com>
2024-12-10 14:09:43 -0500

    update new_todo.js

---
+++ scripts/api.js
@@ -0,0 +1,8 @@
+"use strict"
+async function getData(type = "todos" , id = "") {
+    return fetch(`http://localhost:8083/api/${type}/${id}`)
+      .then((response) => response.json())
+      .then((data) => data)
+      .catch((error) => error);
+  }
+  
